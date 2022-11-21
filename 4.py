33cc338fdaf5bc32d9c14b0c7fc4856f5a234b5c
vijay1951 <60980946+vijay1951@users.noreply.github.com>
2022-11-21 17:04:58 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,38 +1,39 @@
+
 def form(arr,i,j):
     if arr[i][j]==0:
             if len(tl)==0:
-                tl.append(i)
-                tl.append(j)
+                topl.append(i)
+                topl.append(j)
             else:
-                tl[0]=min(i,tl[0])
-                tl[1]=min(j,tl[1])
+                topl[0]=min(i,topl[0])
+                topl[1]=min(j,topl[1])
             if len(tr)==0:
-                tr.append(i)
-                tr.append(j)
+                topr.append(i)
+                topr.append(j)
             else:
-                tr[0]=min(i,tr[0])
-                tr[1]=max(j,tr[1])
-            if len(bl)==0:
-                bl.append(i)
-                bl.append(j)
+                topr[0]=min(i,topr[0])
+                topr[1]=max(j,topr[1])
+            if len(bottoml)==0:
+                bottoml.append(i)
+                bottoml.append(j)
             else:
-                bl[0]=max(i,bl[0])
-                bl[1]=min(j,bl[1])
-            if len(br)==0:
-                br.append(i)
-                br.append(j)
+                bottoml[0]=max(i,bottoml[0])
+                bottoml[1]=min(j,bottoml[1])
+            if len(bottomr)==0:
+                bottomr.append(i)
+                bottomr.append(j)
             else:
-                br[0]=max(i,br[0])
-                br[1]=max(j,br[1])
+                bottomr[0]=max(i,bottomr[0])
+                bottomr[1]=max(j,bottomr[1])
 arr=[]
 for i in range(256):
     l1=list(map(int,input().split(" ")))
     arr.append(l1)
-tl=[]
-tr=[]
-bl=[]
-br=[]
+topl=[]
+topr=[]
+bottoml=[]
+bottomr=[]
 for i in range(256):
     for j in range(256):
         form(arr,i,j)
-print(tuple(tl),tuple(tr),tuple(bl),tuple(br))
+print(tuple(topl),tuple(topr),tuple(bottoml),tuple(bottomr))
