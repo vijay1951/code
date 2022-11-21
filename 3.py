1e14fcd28764e40669c54edd457ccf456e9789b2
vijay1951 <60980946+vijay1951@users.noreply.github.com>
2022-11-21 16:05:49 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,18 @@
+def check(a) :
+	for i in range(len(a)) :
+		if (a[i] == '4' or a[i] == '6' or a[i] == '7') :
+			return False;
+	return True;
+
+n=int(input())
+count=0
+l1=[]
+for i in range(1,1000000):
+    if (check(str(i))) :
+        count+=1
+        l1.append(i)
+print(l1[n-1])
+	    
+
+
 
