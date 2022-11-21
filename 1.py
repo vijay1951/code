1d3fb8ea524af72b953bf7c53cda021d52011233
vijay1951 <60980946+vijay1951@users.noreply.github.com>
2022-11-21 15:45:17 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,15 @@
-
+a=int(input())
+if a<0:
+    s1="-"
+else:
+    s1=" "
+a=abs(a) 
+if a<3:
+    print(int(a))
+    exit(0)
+s=''
+while a!=0:
+    s=str(a%3)+s 
+    a=a//3
+print(int(s1+s))    
+    
